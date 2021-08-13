eaa932d1743cb2fbbbac3aab3c30be27a364f625
sarzixon <msarzala.work@gmail.com>
2021-08-13 23:40:27 +0200

    created Github api Auth

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
-const express = require('express');
+require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+const path = require('path');
 
 const app = express();
+
+
+app.get('/', (req,res) => {
+
+    res.sendFile(path.resolve(__dirname,'index.html'));
+
+});
+
+app.get('/callback', (req,res) => {
+
+    axios.post("https://github.com/login/oauth/access_token",{
+        client_id : process.env.CLIENT_ID,
+        client_secret : process.env.CLIENT_SECRET,
+        code: req.query.code
+        }
+    
+    ).then((response) => {
+        res.send(response.data)
+         console.log(response.data);
+    }).catch(e => {
+        res.send(e)
+        console.log(e);
+    })
+     
+
+
+});
+
+
+
+app.listen(3000);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,20 +9,24 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.bundle.js',
       },
-      module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }
-        ]
-      }
-      
+      // module: {
+      //   rules: [
+      //     {
+      //       test: /\.m?js$/,
+      //       exclude: /(node_modules|bower_components)/,
+      //       use: {
+      //         loader: 'babel-loader',
+      //         options: {
+      //           presets: ['@babel/preset-env']
+      //         }
+      //       }
+      //     }
+      //   ]
+      // },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: 'src/index.html'
+        })
+      ]
   };
   
